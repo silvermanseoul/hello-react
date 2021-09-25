ecd0893d0f065f9fb8385bca793fecebe7bcea56
silvermanseoul <silverman.unicorn@gmail.com>
2021-09-25 17:40:50 +0900

    feat: 승자 결정하기 위해 calculateWinner() 추가

--- src/index.js
+++ src/index.js
@@ -22,6 +22,12 @@ class Board extends React.Component {
   handleClick(i) {
     // 불변성 유지
     const squares = this.state.squares.slice();
+
+    // 승자가 결정됐거나 Square가 채워져 있다면 클릭 무시
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({ squares: squares, xIsNext: !this.state.xIsNext });
   }
@@ -36,7 +42,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -75,6 +87,26 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // ========================================
